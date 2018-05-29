cc23b577cf550211bd815ca8cc0e70e6a5bce5cc
texot <tete1030@gmail.com>
2018-05-29 20:34:36 +0800

    add 1.9.4 build for my own use

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-version=1.9.3
+version=1.9.4
 rm builds/zotero-scholar-citations-${version}-fx.xpi
 zip -r builds/zotero-scholar-citations-${version}-fx.xpi chrome/* chrome.manifest install.rdf
