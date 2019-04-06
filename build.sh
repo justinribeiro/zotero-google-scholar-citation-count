631e3ea38410d064d19660d70d62392dab2a0bbd
Max Kuehn <1742898+MaxKuehn@users.noreply.github.com>
2019-04-06 23:51:09 +0200

    Fix build process
    * Use snapshot versions to distinguish dev builds
    * Add the most primitive clean script ever

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-
-version=1.9.4
-rm build -rf
-if [ -z $DEBUG ]; then
-	zip -r builds/zotero-scholar-citations-${version}-fx.xpi chrome/* chrome.manifest install.rdf
-else
-	zip -r builds/zotero-scholar-citations-${version}-debug.xpi chrome/* chrome.manifest install.rdf
-fi
+version='1.9.5-snapshot'
+mkdir build
+zip -r build/zotero-scholar-citations-${version}-fx.xpi chrome/* chrome.manifest install.rdf
