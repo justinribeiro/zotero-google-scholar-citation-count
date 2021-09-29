91dbc1f59651943e9a5e3f14eba30e7fea415b66
Justin Ribeiro <justin@justinribeiro.com>
2021-09-29 14:07:03 -0700

    refactor: build script now patch rdf, deps updated

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-version=$npm_package_version
+
+# set the version for our file
+# requires NPM 7.20+
+version=$(npm pkg get version | tr -d '"')
+
+# patch the RDF file version
+sed -i "s/^.*em:version.*$/        em:version=\"${version}\"/" install.rdf
+
 mkdir -p build
 zip -r build/zotero-scholar-citations-${version}-fx.xpi chrome/* chrome.manifest install.rdf
