e1bb2a4a9ecbc94ae8fcfc060c5ddb5e36c7c00a
Justin Ribeiro <justin@justinribeiro.com>
2024-05-20 11:18:00 -0700

    feat: new build scripts

--- build.sh
+++ build.sh
@@ -4,10 +4,19 @@
 # requires NPM 7.20+
 version=$(npm pkg get version | tr -d '"')
 
-# patch the RDF file version
-#sed -i "s/^.*em:version.*$/        em:version=\"${version}\"/" install.rdf
-
 rm -rf build
 mkdir -p build
 cd src
 zip -r ../build/zotero-google-scholar-citation-count-${version}.xpi *
+cd ..
+
+# patch the JSON file version
+jq --arg version "$version" '.addons."justin@justinribeiro.com".updates.[0].version |= "\($version)"' updates.json | sponge updates.json
+
+# patch the update link
+updatelink="https://github.com/justinribeiro/zotero-google-scholar-citation-count/releases/download/v${version}/zotero-google-scholar-citation-count-${version}.xpi"
+jq --arg updatelink "$updatelink" '.addons."justin@justinribeiro.com".updates.[0].update_link |= "\($updatelink)"' updates.json | sponge updates.json
+
+# patch the hash for the XPI
+hash=$(sh -c 'sha256sum < "$1" | cut -d" " -f1' -- ./build/zotero-google-scholar-citation-count-${version}.xpi)
+jq --arg hash "$hash" '.addons."justin@justinribeiro.com".updates.[0].update_hash |= "sha256:\($hash)"' updates.json | sponge updates.json
